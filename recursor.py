35cca562a76eca1030c7c012d499be8464b4366e
betomark <almark16@gmail.com>
2016-09-13 22:22:47 +0200

    Create recursor.py & defining initial structure
    In this commit basic project structure is defined and explained

---
+++ recursor.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+def recursor(recursion_term_init, base_cases, terms_update=[-1], terms_operations=[], recursive_step_function):
+	"""
+	This function will return recursive computation output
+	"""
+	pass
+
+def store_function_result():
+	"""
+	This function will store a computed result of a recursive step
+	"""
+	pass
+
+def purge_not_needed_results():
+	"""
+	This function it's just for clean resutls dictionary which will not be needed for further computations steps
+	"""
+	pass
+
+def generate_function_dictionary():
+	"""
+	This function will generate all needed function hierarchy in a dictionay,
+	indexed by integer numbers in increasing index depending on their order of computation
+	"""
+	pass
