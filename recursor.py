2381157aa7a54ca9b03f098a72159b0e089d220c
betomark <almark16@gmail.com>
2016-09-14 00:31:22 +0200

    first approach of generate_function_dictionary
    This commit adds:
    -Construction of two dictionaries (which will store the calculus structure) in recursor
    -First approach to generate_function_dictionary

--- recursor.py
+++ recursor.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-def recursor(recursion_term_init, base_cases, terms_update=[-1], terms_operations=[], recursive_step_function):
+def recursor(recursion_term_init, base_cases, recursive_step_function, terms_update=[lambda x: x - 1], terms_operations=[]):
 	"""
 	This function will return recursive computation output
 	"""
-	pass
+	results_dictionary = {i + 1: case for i, case in enumerate(base_cases)}
+	function_dictionary = generate_function_dictionary(recursion_term_init, recursive_step_function, terms_update, terms_operations)
 
 def store_function_result():
 	"""
@@ -18,9 +19,22 @@ def purge_not_needed_results():
 	"""
 	pass
 
-def generate_function_dictionary():
+def generate_function_dictionary(r_t_init, r_s_function, terms_update, terms_operations):
 	"""
 	This function will generate all needed function hierarchy in a dictionay,
 	indexed by integer numbers in increasing index depending on their order of computation
 	"""
-	pass
+	def next_step_update(terms_update):
+		return [update(terms_update[i]) for i, update in enumerate(terms_update)]
+		
+	function_dictionary = {}
+	index = 1
+	next_terms = [update(r_t_init) for update in terms_update]
+	while min(next_terms) > 0:
+		'''
+		dictionary construction
+		'''
+		terms_update = next_step_update(terms_update)
+		next_terms = [update(r_t_init) for update in terms_update]
+		index += 1
+	return function_dictionary
