5b1f768f9cecd5d6ece39feef88280d5d1358ee5
betomark <almark16@gmail.com>
2016-09-15 00:48:39 +0200

    Change of the initial approach
    In this commit I've change initial parameters for recursion, now the entire function must be expressed in a string format where each recursive call have to follow the next format:
    [<term update>]
    Also I've implemented the collateral changes in generate_function_dictionary

--- recursor.py
+++ recursor.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+from ast import literal_eval
+from copy import copy
 
-def recursor(recursion_term_init, base_cases, recursive_step_function, terms_update=[lambda x: x - 1], terms_operations=[]):
+def recursor(recursion_term_init, base_cases, recursive_step_function):
 	"""
 	This function will return recursive computation output
 	"""
 	results_dictionary = {i + 1: case for i, case in enumerate(base_cases)}
-	function_dictionary = generate_function_dictionary(recursion_term_init, recursive_step_function, terms_update, terms_operations)
+	function_dictionary = generate_function_dictionary(recursion_term_init, recursive_step_function)
 
 def store_function_result():
 	"""
@@ -19,7 +21,7 @@ def purge_not_needed_results():
 	"""
 	pass
 
-def generate_function_dictionary(r_t_init, r_s_function, terms_update, terms_operations):
+def generate_function_dictionary(r_t_init, r_s_function):
 	"""
 	This function will generate all needed function hierarchy in a dictionay,
 	indexed by integer numbers in increasing index depending on their order of computation
@@ -27,24 +29,28 @@ def generate_function_dictionary(r_t_init, r_s_function, terms_update, terms_ope
 	def next_step_update(terms_update):
 		return [update(terms_update[i]) for i, update in enumerate(terms_update)]
 		
+	def extract_terms_update(r_s_function):
+		terms_update = []
+		split = copy(r_s_functions).split('[').split(']')
+		next_is_term_updater = False
+		for function_slice in split:
+			if next_is_term_updater:
+				terms_update.append(lambda x: literal_eval(function_sice.replace('x', '{}').format(x)))
+				next_is_term_updater = False
+				r_s_function.replace('[' + function_slice + ']', 'results_dictionary[{}]')
+			else:
+				if '[' in function_slice:
+					next_is_term_updater = True
+		return terms_update
+	
+	terms_update = extract_terms_update(r_s_function)
 	function_dictionary = {}
 	index = 1
 	next_terms = [update(r_t_init) for update in terms_update]
-	if terms_operations is []:
-		while min(next_terms) > 0:
-		
-			function_dictionary[index] = "results_dictionary[{}] {}".format(index, r_s_function) 
-			terms_update = next_step_update(terms_update)
-			next_terms = [update(r_t_init) for update in terms_update]
-			index += 1
-	else:
-		while min(next_terms) > 0:
-			to_string = "results_dictionary[{}] ".format(next_terms[0])
-			for i, operation in enumerate(terms_operations):
-				to_string += operation + "results_dictionary[{}] ".format(next_terms[i + 1])
-			function_dictionary[index] = to_string + r_s_function
-			terms_update = next_step_update(terms_update)
-			next_terms = [update(r_t_init) for update in terms_update]
-			index += 1
+	while min(next_terms) > 0:
+		function_dictionary[index] = r_s_function.format(*next_terms) 
+		terms_update = next_step_update(terms_update)
+		next_terms = [update(r_t_init) for update in terms_update]
+		index += 1
 	
 	return function_dictionary
