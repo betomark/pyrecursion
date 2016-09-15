92dd3a8a96ad363c2929afdc7c207b2dbfdd054a
betomark <almark16@gmail.com>
2016-09-15 21:30:04 +0200

    missing indent

--- recursor.py
+++ recursor.py
@@ -15,42 +15,41 @@ class recursion():
 		
 	return exec("self.res{}".format(recursion_term_init))
 
-    
-def purge_not_needed_results():
-	"""
-	This function it's just for clean resutls dictionary which will not be needed for further computations steps
-	"""
-	# exec("del self.res{}".format())
-	pass
-
-def generate_function_dictionary(r_t_init, r_s_function):
-	"""
-	This function will generate all needed function hierarchy in a dictionay,
-	indexed by integer numbers in increasing index depending on their order of computation
-	"""
-	def next_step_update(terms_update):
-		return [update(terms_update[i]) for i, update in enumerate(terms_update)]
+	def purge_not_needed_results(self):
+		"""
+		This function it's just for clean resutls dictionary which will not be needed for further computations steps
+		"""
+		# exec("del self.res{}".format())
+		pass
+	
+	def generate_function_dictionary(self, r_t_init, r_s_function):
+		"""
+		This function will generate all needed function hierarchy in a dictionay,
+		indexed by integer numbers in increasing index depending on their order of computation
+		"""
+		def next_step_update(terms_update):
+			return [update(terms_update[i]) for i, update in enumerate(terms_update)]
+			
+		def extract_terms_update(r_s_function):
+			terms_update = []
+			split = copy(r_s_functions).split('[').split(']')
+			next_is_term_updater = False
+			for function_slice in split:
+				if next_is_term_updater:
+					terms_update.append(lambda x: literal_eval(function_sice.replace('x', '{}').format(x)))
+					next_is_term_updater = False
+					r_s_function.replace('[' + function_slice + ']', 'res[{}]')
+				else:
+					if '[' in function_slice:
+						next_is_term_updater = True
+			return terms_update
 		
-	def extract_terms_update(r_s_function):
-		terms_update = []
-		split = copy(r_s_functions).split('[').split(']')
-		next_is_term_updater = False
-		for function_slice in split:
-			if next_is_term_updater:
-				terms_update.append(lambda x: literal_eval(function_sice.replace('x', '{}').format(x)))
-				next_is_term_updater = False
-				r_s_function.replace('[' + function_slice + ']', 'res[{}]')
-			else:
-				if '[' in function_slice:
-					next_is_term_updater = True
-		return terms_update
-	
-	terms_update = extract_terms_update(r_s_function)
-	index = 1
-	next_terms = [update(r_t_init) for update in terms_update]
-	while min(next_terms) > 0:
-		exec("self.res{} = r_s_function.format(*next_terms) ".format())
-		terms_update = next_step_update(terms_update)
+		terms_update = extract_terms_update(r_s_function)
+		index = 1
 		next_terms = [update(r_t_init) for update in terms_update]
-		index += 1
-	
+		while min(next_terms) > 0:
+			exec("self.res{} = r_s_function.format(*next_terms) ".format())
+			terms_update = next_step_update(terms_update)
+			next_terms = [update(r_t_init) for update in terms_update]
+			index += 1
+		
