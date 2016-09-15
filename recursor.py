cad74d8daada2133b6569bc879ad15e6697fbc4f
betomark <almark16@gmail.com>
2016-09-15 21:29:00 +0200

    corrupting code with clases

--- recursor.py
+++ recursor.py
@@ -2,22 +2,25 @@
 from ast import literal_eval
 from copy import copy
 
-def recursor(recursion_term_init, base_cases, recursive_step_function):
+class recursion():
+    def __init__(self, recursion_term_init, base_cases, recursive_step_function): 
 	"""
 	This function will return recursive computation output
 	"""
-	results_dictionary = base_cases
-	function_dictionary = generate_function_dictionary(recursion_term_init, recursive_step_function)
-	for function_index in map(lambda x: x+1, range(max(function_dictionary.keys()).reverse()):
-		results_dictionary[] = function_dictionary[function_index]  # TODO: results_dictionary index
-		del function_dictionary[function_index]
-	return results_dictionary[recursion_term_init]
+	for i in base_cases:
+            exec("self.res{} = {}".format(i, base_cases[i]))
+	generate_function_variables(recursion_term_init, recursive_step_function)
+	for result_index in :
+		exec("exec(self.res{})".format(result_index)
+		
+	return exec("self.res{}".format(recursion_term_init))
 
-
+    
 def purge_not_needed_results():
 	"""
 	This function it's just for clean resutls dictionary which will not be needed for further computations steps
 	"""
+	# exec("del self.res{}".format())
 	pass
 
 def generate_function_dictionary(r_t_init, r_s_function):
@@ -36,20 +39,18 @@ def generate_function_dictionary(r_t_init, r_s_function):
 			if next_is_term_updater:
 				terms_update.append(lambda x: literal_eval(function_sice.replace('x', '{}').format(x)))
 				next_is_term_updater = False
-				r_s_function.replace('[' + function_slice + ']', 'results_dictionary[{}]')
+				r_s_function.replace('[' + function_slice + ']', 'res[{}]')
 			else:
 				if '[' in function_slice:
 					next_is_term_updater = True
 		return terms_update
 	
 	terms_update = extract_terms_update(r_s_function)
-	function_dictionary = {}
 	index = 1
 	next_terms = [update(r_t_init) for update in terms_update]
 	while min(next_terms) > 0:
-		function_dictionary[index] = r_s_function.format(*next_terms) 
+		exec("self.res{} = r_s_function.format(*next_terms) ".format())
 		terms_update = next_step_update(terms_update)
 		next_terms = [update(r_t_init) for update in terms_update]
 		index += 1
 	
-	return function_dictionary
